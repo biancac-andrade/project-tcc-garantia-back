6773a75e9d99406e77fdb69a4a06f3c178d8c14d
Bianca Andrade <bianca.andrade@ab-inbev.com>
2023-09-04 20:52:06 -0300

    nomea a rota post de product

--- routes/product.js
+++ routes/product.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get('/', productController.getAllProducts);
 router.get('/:id', productController.getProductById);
 router.post('/', productController.createProduct);
-router.post("/", productController.createSeveralProduct);
+router.post("/bulk", productController.createSeveralProduct);
 router.put('/:id', productController.updateProduct);
 router.delete('/:id', productController.deleteProduct);
 router.delete('/', productController.deleteAllProducts);
